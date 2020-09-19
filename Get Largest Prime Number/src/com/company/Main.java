26ceacdee71ec2864cf233a0708513622daad19b
Jabz259 <45884317+Jabz259@users.noreply.github.com>
2020-09-19 22:16:24 +0100

    A much more efficient program
    Made validation a little more efficient and simple, also worked on the loops and improved logic, one major change has been adding 2 loops instead of 3, and using boolean variable to confirm the validity of a prime number.

--- Get Largest Prime Number/src/com/company/Main.java
+++ Get Largest Prime Number/src/com/company/Main.java
@@ -8,69 +8,50 @@ public class Main {
 
     }
 
+    public static int getLargestPrime (int number) {
 
-    //hey
-    public static int getLargestPrime(int number) {
-
-        //normal error handling
+        //validating input
         if (number <= 1) {
+            System.out.println("Invalid");
             return -1;
-            //if any numbers lower than 10 is a prime
-        } else if (number < 10 && number % 2 != 0 || number == 2) {
-            System.out.println(number);
-            return number;
         }
-
-        //verifying if the supplied number is just a prime
-        //for larger integers
-        for (int  i = 2; i <=100; i ++) {
+        //creating a variable to store prime numbers
+        int largestPrimNum = 0;
+        //for loop which will test every value with number
+        //so that it is a factor of number
+        for (int i =1; i <= number; i++) {
+            //storing a boolean value
             boolean primeNum = true;
-
-            for (int j = 2; j <i; j++) {
-                if (i%j == 0) {
-                    primeNum = false;
-                    break;
+            //if there is a factor of number then
+            //execute next loop
+            if (number % i == 0) {
+                //beginning from 2, we will test each
+                //successful i value
+                //this will use the modulo operator: i % j until there are no remainders
+                //if there is no remainders
+                //boolean value will be set to false
+                for (int j = 2; j < i; j++) {
+                    //reason why j must be smaller than i
+                    //is because each i value will be testing using modulo remainder (j)
+                    //up until it is at the i value
+                    //example: 5--> 2 % 5 --> 3 % 5 --> 4 % 5 --> = stop
+                    if (i % j == 0) {
+                        //prime num will check if each index of j is a possible divisor
+                        primeNum=false;
+                    }
+                }
+                //if the number does not have a divisor apart from 1 and itself
+                //and it is not false from the previous if statmenet
+                //then store i into largestPrimeNum Variable
+                //return value
+                if(primeNum) {
+                    largestPrimNum = i;
+                    System.out.println(largestPrimNum);
                 }
             }
-            if (primeNum && number == i) {
-                System.out.println(number);
-                return number;
-            }
         }
-
-        //count variable
-        int count = 0;
-        //creating first loop
-        for (int i = 0; i < 100; i++) {
-            //incrementing count
-            count++;
-            //checking the factors of number
-            if (number % count == 0) {
-                //if there are multiple factors then we
-                //will check and see if there any prime numbers
-                //which can make up the number again e.g 3 x 7 = 21
-                 for (int j = 2; j < i; j ++) {
-                     //if count x j not equal to number then it is not a prime number
-                     if (count * j != number){
-                         continue;
-                     //otherwise it is fine to print and return
-                     } else {
-
-                         System.out.println(count);
-                         return count;
-                         }
-
-
-                     }
-                 }
-
-            }
-
-        return -1;
-        }
-
+        return largestPrimNum;
 
     }
 
-
-
+}
